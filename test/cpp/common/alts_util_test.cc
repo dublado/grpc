9e97f7f580ec954c876945d9768ac53fa70738e0
ZhenLian <zhenlian@google.com>
2020-08-15 17:04:24 -0700

    fix MSAN error

--- test/cpp/common/alts_util_test.cc
+++ test/cpp/common/alts_util_test.cc
@@ -211,6 +211,7 @@ TEST(AltsUtilTest, AltsClientAuthzCheck) {
 }  // namespace grpc
 
 int main(int argc, char** argv) {
+  grpc::testing::TestEnvironment env(argc, argv);
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
